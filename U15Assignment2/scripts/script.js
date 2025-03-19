812b13001c11a7ed715e96bfa740770d5529aa05
jyx-9 <JXu@tmc.ac.uk>
2025-03-19 10:25:28 +0000

    Add files via upload

---
+++ U15Assignment2/scripts/script.js
@@ -0,0 +1,6 @@
+/* JavaScript File (scripts/script.js) */
+
+        // JavaScript to handle form submission
+document.getElementById("myForm").onsubmit = function() {
+    alert("Your information has been successfully received! You will receive the paper copy.");
+};
