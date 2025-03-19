01100a7ad33540e9c1554e8a85751c3d1c6cb290
jyx-9 <JXu@tmc.ac.uk>
2025-03-19 10:31:00 +0000

    Delete U15Assignment2 directory

--- U15Assignment2/scripts/script.js
+++
@@ -1,6 +0,0 @@
-/* JavaScript File (scripts/script.js) */
-
-        // JavaScript to handle form submission
-document.getElementById("myForm").onsubmit = function() {
-    alert("Your information has been successfully received! You will receive the paper copy.");
-};
